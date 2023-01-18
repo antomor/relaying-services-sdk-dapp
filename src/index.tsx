1aff050a4ae2751a2dcb8ffa41fbcad3dea2d802
Antonio Morrone <antonio@iovlabs.org>
2023-01-18 09:11:06 +0100

    fix: remove reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@
 import ReactDOM from 'react-dom/client';
 import 'src/index.css';
 import App from 'src/App';
-import reportWebVitals from 'src/reportWebVitals';
 import { StoreProvider } from './context/context';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
@@ -17,8 +16,3 @@ root.render(
         </StoreProvider>
     //</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
